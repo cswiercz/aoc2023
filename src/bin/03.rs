d192752043a18e41215a19778c0b139d15e6fa4f
Chris Swierczewski <cswiercz@gmail.com>
2023-12-04 22:44:23 +0000

    Day 03 Part 1

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 advent_of_code::solution!(3);
 
@@ -62,6 +62,8 @@ pub fn part_one(input: &str) -> Option<u32> {
     let mut answer: u32 = 0;
 
     for i in 0..nrows {
+        current_number.clear();
+        is_adjacent = false;
         for j in 0..ncols {
             if let Symbol::Digit(c) = grid[i][j] {
                 current_number.push(c);
@@ -72,7 +74,6 @@ pub fn part_one(input: &str) -> Option<u32> {
                     );
             } else {
                 if !current_number.is_empty() & is_adjacent {
-                    println!("added: {}", current_number);
                     answer += current_number.as_str().parse::<u32>().unwrap();
                 }
                 current_number.clear();
@@ -90,22 +91,49 @@ pub fn part_two(input: &str) -> Option<u32> {
     let nrows = grid.len();
     let ncols = grid[0].len();
 
-    let mut gears: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
+    let mut gears: HashMap<(usize, usize), HashSet<u32>> = HashMap::new();
     let mut current_number: String = String::new();
-    let mut is_adjacent_to_gear = false;
-    let mut answer = 0;
+    let mut current_adjacent_gears: HashSet<(usize, usize)> = HashSet::new();
 
     for i in 0..nrows {
         for j in 0..ncols {
             if let Symbol::Digit(c) = grid[i][j] {
                 current_number.push(c);
-                is_adjacent_to_gear |= neighbors(&grid, i, j)
+                neighbors(&grid, i, j)
                     .iter()
-                    .any(|(u, v)|
-                        (grid[*u][*v] == Symbol::Symbol) | (grid[*u][*v] == Symbol::Gear)
-                    );
+                    .filter(|(u, v)| (grid[*u][*v] == Symbol::Gear))
+                    .for_each(|(u, v)| _ = current_adjacent_gears.insert((*u, *v)));
+            } else {
+                if !current_number.is_empty() {
+                    let n = current_number.parse::<u32>().unwrap();
+                    for (u, v) in current_adjacent_gears.iter() {
+                        let key = &(*u, *v);
+                        if gears.contains_key(key) {
+                            gears.get_mut(key).unwrap().insert(n);
+                        } else {
+                            let mut s = HashSet::new();
+                            s.insert(n);
+                            gears.insert(*key, s);
+                        }
+                    }
+                }
+                current_number.clear();
+                current_adjacent_gears.clear();
+                continue;
+            }
         }
     }
+
+    // gears now contains coordinate -> [adjacent part numbers]
+    let answer = gears
+        .iter()
+//        .inspect(|(coord, nums)| println!("{:?}: {:?}", coord, nums))
+        .filter(|(_, nums)| nums.len() == 2)
+        .inspect(|(coord, nums)| println!("{:?}:\t {:?}", coord, nums))
+        .map(|(_, nums)| nums.iter().product::<u32>())
+        .sum();
+
+    Some(answer)
 }
 
 #[cfg(test)]
@@ -115,12 +143,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4361));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835));
     }
 }

84d01c6630ee6348cf997899e58268094b85b0d3
Chris Swierczewski <cswiercz@gmail.com>
2023-12-08 11:39:16 -0800

    Day 05 brute force

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,23 +1,21 @@
 advent_of_code::solution!(5);
 
-use core::num;
-use std::collections::{HashMap, VecDeque};
-
 use itertools::Itertools;
 use regex::Regex;
+use std::collections::HashMap;
 
-type Seeds = Vec<u32>;
-type Range = (u32, u32, u32);
+type Seeds = Vec<u64>;
+type Range = (u64, u64, u64);
 type Maps = HashMap<String, Map>;
 
 #[derive(Clone, Debug)]
-struct Map {
+pub struct Map {
     src: String,
     dst: String,
-    ranges: Vec<Range>
+    ranges: Vec<Range>,
 }
 
-fn parse_input(input: &str) -> (Seeds, Maps) {
+pub fn parse_input(input: &str) -> (Seeds, Maps) {
     let re_nums = Regex::new(r"(?:[0-9]+)").unwrap();
     let re_map = Regex::new(r"(?<src>[a-z]+)-to-(?<dst>[a-z]+)").unwrap();
 
@@ -25,26 +23,36 @@ fn parse_input(input: &str) -> (Seeds, Maps) {
         .split_terminator('\n')
         .map(|line| line.trim())
         .peekable();
-    
-    let seeds: Vec<u32> = re_nums
+
+    let seeds: Vec<u64> = re_nums
         .find_iter(lines.next().unwrap())
-        .map(|s| s.as_str().parse::<u32>().unwrap())
+        .map(|s| s.as_str().parse::<u64>().unwrap())
         .collect();
     lines.next();
 
     let mut maps: Maps = HashMap::new();
+
     while lines.peek().is_some() {
         let line = lines.next().unwrap();
         let cap = re_map.captures(line).unwrap();
-        let mut map = Map { src: String::from(&cap["src"]), dst: String::from(&cap["dst"]), ranges: Vec::new() };
+        let mut map = Map {
+            src: String::from(&cap["src"]),
+            dst: String::from(&cap["dst"]),
+            ranges: Vec::new(),
+        };
 
         while let Some(line) = lines.next() {
             if line.is_empty() {
+                // TODO: this is bad
                 let m = map.clone();
                 maps.insert(m.src.clone(), m);
                 break;
             } else {
-                let r: Range = re_nums.find_iter(line).map(|s| s.as_str().parse::<u32>().unwrap()).collect_tuple().unwrap();
+                let r: Range = re_nums
+                    .find_iter(line)
+                    .map(|s| s.as_str().parse::<u64>().unwrap())
+                    .collect_tuple()
+                    .unwrap();
                 map.ranges.push(r);
             }
         }
@@ -53,38 +61,56 @@ fn parse_input(input: &str) -> (Seeds, Maps) {
     (seeds, maps)
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (seeds, maps) = parse_input(input);
-    let num_seeds = seeds.len();
+////////////////////////////////////////////////////////////////////////////////
+/// Solutions
+////////////////////////////////////////////////////////////////////////////////
 
+pub fn propagate_seed(seed: u64, maps: &Maps) -> u64 {
     let mut src = String::from("seed");
-    let mut dst: String;
-    let mut stack: VecDeque<u32> = VecDeque::from(seeds);
-    while src.as_str() != "location" {
-        let map = maps.get(&src).unwrap();
-        println!("stack: {:?}", stack);
-        println!("map:   {:?}", map);
-        println!("\t{} -> {}", src, &map.dst);
-        for _ in 0..num_seeds {
-            let val = stack.pop_front().unwrap();
-            let next_val = map
-                .ranges
-                .iter()
-                .filter(|&&(s, _, l)| (val >= s) & (val < s + l))
-                .inspect()
-                .map(|&(s, d, _)| d + (val - s))
-                .next()
-                .unwrap();
+    let mut dst = String::from("foo");
+    let mut value = seed;
 
-            println!("\t{} -> {}", val, next_val);
-        }
+    while dst != String::from("location") {
+        let ranges = &maps.get(&src).unwrap().ranges;
+        dst = maps.get(&src).unwrap().dst.clone();
+        value = ranges
+            .iter()
+            .filter_map(|(d, s, l)| {
+                if (value >= *s) & (value < *s + *l) {
+                    Some(d + value - s)
+                } else {
+                    None
+                }
+            })
+            .next()
+            .unwrap_or(value);
+        src = dst.clone();
     }
 
-    None
+    value
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (seeds, maps) = parse_input(input);
+
+    seeds
+        .iter()
+        .map(|&s| propagate_seed(s, &maps))
+        .min()
+        .and_then(|v| Some(v as u32))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (seeds, maps) = parse_input(input);
+
+    // brute forcing because I'm done with this problem. be sure to
+    // run with --release flag. took X minutes on my machine.
+    seeds
+        .chunks_exact(2)
+        .flat_map(|pair| (pair[0])..(pair[0] + pair[1]))
+        .map(|s| propagate_seed(s, &maps))
+        .min()
+        .and_then(|v| Some(v as u32))
 }
 
 #[cfg(test)]
@@ -94,12 +120,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(35));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(46));
     }
 }

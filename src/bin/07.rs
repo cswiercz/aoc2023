947db446979b18d4a43ca7650987a617c6c1ede8
Chris Swierczewski <cswiercz@gmail.com>
2023-12-07 18:20:55 -0800

    Day 07

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,32 +1,44 @@
-use std::{collections::HashMap, cmp::Ordering, fmt::Debug};
+use std::{
+    cmp::Ordering,
+    collections::BinaryHeap,
+    fmt::{Debug, Display},
+};
 
 use itertools::Itertools;
 
 advent_of_code::solution!(7);
 
-#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq, PartialOrd)]
+////////////////////////////////////////////////////////////////////////////////
+/// Card and Hand Type
+////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd, Hash)]
 pub enum Rank {
-    Two   = 2,
+    Joker = 0,
+    Two = 2,
     Three = 3,
-    Four  = 4,
-    Five  = 5,
-    Six   = 6,
+    Four = 4,
+    Five = 5,
+    Six = 6,
     Seven = 7,
     Eight = 8,
-    Nine  = 9,
-    Ten   = 10,
-    Jack  = 11,
+    Nine = 9,
+    Ten = 10,
+    Jack = 11,
     Queen = 12,
-    King  = 13,
-    Ace   = 14,
+    King = 13,
+    Ace = 14,
 }
 
-#[derive(Debug, PartialEq, PartialOrd)]
-pub struct Card(Rank);
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+pub struct Card {
+    rank: Rank,
+}
 
 impl Card {
     pub fn from(c: &char) -> Card {
         let rank = match *c {
+            '@' => Rank::Joker,
             '2' => Rank::Two,
             '3' => Rank::Three,
             '4' => Rank::Four,
@@ -42,124 +54,117 @@ impl Card {
             'A' => Rank::Ace,
             _ => unreachable!("Invalid character: {}", c),
         };
-        Card(rank)
+        Card { rank: rank }
     }
 }
 
-#[derive(Debug, PartialEq, PartialOrd)]
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub enum HandType {
     HighCard = 1,
     OnePair = 2,
     TwoPair = 3,
-    ThreeOfAKind = 4,
+    ThreeKind = 4,
     FullHouse = 5,
-    FourOfAKind = 6,
-    FiveOfAKind = 7,
+    FourKind = 6,
+    FiveKind = 7,
 }
 
-#[derive(Debug)]
-pub struct Hand(Vec<Card>);
+////////////////////////////////////////////////////////////////////////////////
+/// Hand
+////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Hand {
+    cards: Vec<Card>,
+}
 
 impl Hand {
     pub fn from(s: &str) -> Hand {
-        let cards: Vec<Card> = s
-            .chars()
-            .map(|c| Card::from(&c))
-            .collect();
-        Hand(cards)
+        let cards: Vec<Card> = s.chars().map(|c| Card::from(&c)).collect();
+        Hand { cards: cards }
     }
 
     pub fn get_type(&self) -> HandType {
-        let mut rank_counts: HashMap<Rank, u32> = HashMap::new();
-        self.0.iter().for_each(|card| 
-            if let Some(count) = rank_counts.get_mut(&card.0) {
-                *count += 1;
-            } else {
-                rank_counts.insert(card.0, 1);
-            }
-        );
+        let num_jokers = self.cards.iter().filter(|c| c.rank == Rank::Joker).count();
+        let rank_counts = self
+            .cards
+            .iter()
+            .map(|c| c.rank)
+            .filter(|&r| r != Rank::Joker)
+            .counts();
+        let mut heap = BinaryHeap::from_iter(rank_counts.values());
+        let first = *heap.pop().unwrap_or(&0) + num_jokers;
+        let second = *heap.pop().unwrap_or(&0);
 
-        // special handling of two-pair detection
-        let counts = rank_counts.values().map(|&v| v).collect_vec();
-        let mut has_pair = false;
-        let mut has_two_pair = false;
-        counts.iter().for_each(|&v| if (v == 2) & has_pair { has_two_pair = true; } else if v == 2 { has_pair = true; });
-
-        if counts.contains(&5) {
-            return HandType::FiveOfAKind;
-        } else if counts.contains(&4) {
-            return HandType::FourOfAKind;
-        } else if counts.contains(&3) & has_pair {
-            return HandType::FullHouse;
-        } else if counts.contains(&3) {
-            return HandType::ThreeOfAKind;
-        } else if has_two_pair {
-            return HandType::TwoPair;
-        } else if has_pair {
-            return HandType::OnePair;
-        } else {
-            return HandType::HighCard;
+        match (first, second) {
+            (5, _) => HandType::FiveKind,
+            (4, _) => HandType::FourKind,
+            (3, 2) => HandType::FullHouse,
+            (3, _) => HandType::ThreeKind,
+            (2, 2) => HandType::TwoPair,
+            (2, _) => HandType::OnePair,
+            _ => HandType::HighCard,
         }
     }
 }
-
-impl PartialEq for Hand {
-    fn eq(&self, other: &Hand) -> bool {
-        self.get_type() == other.get_type()
-    }
-}
-
-impl Eq for Hand {}
 
 impl PartialOrd for Hand {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        let self_type = self.get_type();
-        let other_type = other.get_type();
-
-        // use first card rank rule if hand types are equal (e.g 3333A > 2AAAA
-        // and 77888 > 77788 [third card is higher])
-        if self_type == other_type {
-            for (l, r) in self.0.iter().zip(other.0.iter()) {
-                if l != r { return l.partial_cmp(r); }
-            }
-            return None
-        }
-        self_type.partial_cmp(&other_type)
+        Some(self.cmp(other))
     }
 }
 
 impl Ord for Hand {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.partial_cmp(other).unwrap()
+        let self_type = self.get_type();
+        let other_type = other.get_type();
+
+        // use ordered card values to compare when types are equal
+        if self_type == other_type {
+            for (l, r) in self.cards.iter().zip(other.cards.iter()) {
+                match l.cmp(&r) {
+                    Ordering::Equal => continue,
+                    Ordering::Less => return Ordering::Less,
+                    Ordering::Greater => return Ordering::Greater,
+                }
+            }
+        }
+        self_type.cmp(&other_type)
     }
 }
 
+impl Display for Hand {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = self.cards.iter().map(|c| format!("{:?}", c.rank)).join(" ");
+        write!(f, "{}", s)
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
-///
+/// Solutions
 ////////////////////////////////////////////////////////////////////////////////
-pub fn part_one(input: &str) -> Option<u32> {
-    let (mut hands, bids): (Vec<Hand>, Vec<u32>) = input
+
+pub fn solve(input: &str) -> u32 {
+    let mut hands_and_bids: Vec<(Hand, u32)> = input
         .split('\n')
         .map(|line| line.split_once(' ').unwrap())
         .map(|(hand, bid)| (Hand::from(hand), bid.parse::<u32>().unwrap()))
-        .unzip();
+        .collect();
 
-    let mut ranks = (0..hands.len()).collect::<Vec<usize>>();
-    ranks.sort_by_key(|&i| &hands[i]);
+    hands_and_bids.sort_unstable_by(|(l, _), (r, _)| l.cmp(r));
+    hands_and_bids
+        .iter()
+        .enumerate()
+        .map(|(i, (_, bid))| bid * (i as u32 + 1))
+        .sum()
+}
 
-    hands.sort();
-    hands.iter().for_each(|hand| println!("{:?}\t{:?}", hand.get_type(), hand));
-
-    let answer = ranks.iter().zip(bids.iter())
-//        .inspect(|(rank, bid)| println!("rank={}\tbid={}", **rank+1, bid))
-        .map(|(&rank, &bid)| (rank as u32 + 1) * bid)
-        .sum();
-    Some(answer)
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(solve(input))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    Some(solve(input.replace('J', "@").as_str()))
 }
 
 #[cfg(test)]
@@ -169,18 +174,18 @@ mod tests {
     #[test]
     fn test_cards() {
         let c1 = Card::from(&'A');
-        let c2 = Card::from (&'K');
+        let c2 = Card::from(&'K');
         assert!(c1 > c2);
     }
 
     #[test]
     fn test_hands() {
         let h = Hand::from(&"TTT98");
-        assert!(h.get_type() == HandType::ThreeOfAKind);
+        assert!(h.get_type() == HandType::ThreeKind);
 
         let h = Hand::from(&"23432");
         assert!(h.get_type() == HandType::TwoPair);
-        
+
         let h = Hand::from(&"A23A4");
         assert!(h.get_type() == HandType::OnePair);
 
@@ -189,8 +194,8 @@ mod tests {
 
         let h1 = Hand::from(&"33332");
         let h2 = Hand::from(&"2AAAA");
-        assert!(h1.get_type() == HandType::FourOfAKind);
-        assert!(h2.get_type() == HandType::FourOfAKind);
+        assert!(h1.get_type() == HandType::FourKind);
+        assert!(h2.get_type() == HandType::FourKind);
         assert!(h1 > h2);
 
         let h1 = Hand::from(&"77888");
@@ -198,6 +203,36 @@ mod tests {
         assert!(h1.get_type() == HandType::FullHouse);
         assert!(h2.get_type() == HandType::FullHouse);
         assert!(h1 > h2);
+
+        let h = Hand::from(&"22222");
+        assert!(h.get_type() == HandType::FiveKind);
+    }
+
+    #[test]
+    fn test_jokers() {
+        let h1 = Hand::from(&"T55@5");
+        assert!(h1.get_type() == HandType::FourKind);
+
+        let h2 = Hand::from(&"QQQ@A");
+        assert!(h2.get_type() == HandType::FourKind);
+
+        let h3 = Hand::from(&"KT@@T");
+        assert!(h3.get_type() == HandType::FourKind);
+
+        let h4 = Hand::from(&"@@@@@");
+        assert!(h4.get_type() == HandType::FiveKind);
+
+        let h5 = Hand::from(&"@@@@K");
+        assert!(h5.get_type() == HandType::FiveKind);
+
+        let h6 = Hand::from(&"@@@@A");
+        assert!(h6.get_type() == HandType::FiveKind);
+
+        assert!(h1 < h2);
+        assert!(h2 < h3);
+        assert!(h3 < h4);
+        assert!(h4 < h5);
+        assert!(h5 < h6);
     }
 
     #[test]
@@ -209,6 +244,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(5905));
     }
 }

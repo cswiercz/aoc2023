9519591b2d59f1ba1211e612cb024b41527389e0
Chris Swierczewski <cswiercz@gmail.com>
2023-12-15 13:07:58 -0800

    Day 13 Part 2 (in progress)

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -15,27 +15,35 @@ pub fn transpose(grid: &Grid) -> Grid {
         .collect()
 }
 
-pub fn get_reflection_column(grid: &Grid) -> Option<usize> {
+pub fn get_reflection_column(grid: &Grid, part_two: bool) -> Option<usize> {
     let ncols = grid[0].len();
     for col in 1..ncols {
         let length = usize::min(col, ncols - col);
-        let valid_reflection = grid.iter().all(|row| {
-            row[col - length..col]
-                .iter()
-                .zip(row[col..col + length].iter().rev())
-                .all(|(xi, yi)| xi == yi)
-        });
-        if valid_reflection {
+        let error_count = grid
+            .iter()
+            .map(|row| {
+                row[col - length..col]
+                    .iter()
+                    .zip(row[col..col + length].iter().rev())
+                    .map(|(xi, yi)| (xi != yi) as u32)
+                    .sum::<u32>()
+            })
+            .sum::<u32>();
+        if part_two & (error_count == 1) {
+            return Some(col);
+        } else if error_count == 0 {
             return Some(col);
         }
     }
     None
 }
 
-pub fn get_note(grid: &Grid) -> usize {
-    if let Some(note) = get_reflection_column(grid) {
+pub fn get_note(grid: &Grid, part_two: bool) -> usize {
+    if let Some(note) = get_reflection_column(grid, part_two) {
+        println!("COL: {}", note);
         return note;
-    } else if let Some(note) = get_reflection_column(&transpose(grid)) {
+    } else if let Some(note) = get_reflection_column(&transpose(grid), part_two) {
+        println!("ROW: {}", note);
         return 100 * note;
     }
     unreachable!("expected reflection column or row\n{}", to_string(&grid));
@@ -50,14 +58,25 @@ pub fn part_one(input: &str) -> Option<usize> {
                 .map(|l| l.chars().collect::<Vec<char>>())
                 .collect::<Grid>()
         })
-        .map(|grid| get_note(&grid))
+        .map(|grid| get_note(&grid, false))
         .sum();
 
     Some(answer)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let answer = input
+        .split("\n\n")
+        .map(|chunk| {
+            chunk
+                .lines()
+                .map(|l| l.chars().collect::<Vec<char>>())
+                .collect::<Grid>()
+        })
+        .map(|grid| get_note(&grid, true))
+        .sum();
+
+    Some(answer)
 }
 
 #[cfg(test)]
@@ -73,6 +92,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(400));
     }
 }

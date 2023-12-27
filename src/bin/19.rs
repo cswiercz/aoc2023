1fea96d2d5b58bf8440dc941894ed00b879ae496
Chris Swierczewski <cswiercz@gmail.com>
2023-12-27 17:27:13 +0000

    Day 19 Part 1

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -3,11 +3,24 @@ use std::collections::HashMap;
 
 advent_of_code::solution!(19);
 
+#[derive(Debug)]
 pub struct Part {
     x: u32,
     m: u32,
     a: u32,
     s: u32,
+}
+
+impl Part {
+    pub fn from(s: &str) -> Part {
+        let s = s.strip_prefix('{').unwrap().strip_suffix('}').unwrap();
+        let vals: Vec<u32> = s
+            .split(',')
+            .map(|si| si.split_once('=').unwrap().1)
+            .map(|vs| vs.parse::<u32>().unwrap())
+            .collect();
+        Part { x: vals[0], m: vals[1], a: vals[2], s: vals[3] }
+    }
 }
 
 #[derive(Debug)]
@@ -62,6 +75,15 @@ impl Workflow {
         
         Workflow { name: String::from(name), rules: rules, end_dst: String::from(*end_dst) }
     }
+
+    pub fn process(&self, p: &Part) -> String {
+        for rule in self.rules.iter() {
+            if rule.eval(p) {
+                return rule.dst.clone();
+            }
+        }
+        return self.end_dst.clone();
+    }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -76,7 +98,23 @@ pub fn part_one(input: &str) -> Option<u32> {
         .map(Part::from)
         .collect();
 
-    None
+    let accept = String::from("A");
+    let reject = String::from("R");
+    let mut answer = 0;
+
+    for p in parts.iter() {
+        let mut name = String::from("in");
+        while (name != accept) & (name != reject) {
+            let workflow = workflows.get(&name).unwrap();
+            name = workflow.process(p);
+        }
+
+        if name.eq(&accept) {
+            answer += p.x + p.m + p.a + p.s;
+        }
+    }    
+
+    Some(answer)
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {
@@ -99,7 +137,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(19114));
     }
 
     #[test]
